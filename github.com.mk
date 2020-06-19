dcea17a86ff2ecd3feea79cf6931cf197c721078
Arthur Stace <arthur.stace@pm.me>
2020-06-19 02:11:25 +0100

    Squashed 'arthur-stace/backlog/' content from commit 3bc33ff
    git-subtree-dir: arthur-stace/backlog
    git-subtree-split: 3bc33ff3dd0473a99bda9728ba48d6e140cce255

---
+++ github.com.mk
@@ -0,0 +1,6 @@
+DOMAIN = github.com
+COURSE = trekhleb/javascript-algorithms
+SECTIONS = tmp/README.txt
+REMOTE_RESOURCE = https://www.$(DOMAIN)/$(COURSE)/archive/master.zip
+
+include opensource.mk
